bb7cf5eb5ba438adb5237a19c33ffcd7f62d14db
rjbrown2 <91291114+rjbrown2@users.noreply.github.com>
2025-03-25 16:34:16 +0000

    style: auto-format via clang-format

--- fsw/fprime/src/generic_imu_app.h
+++ fsw/fprime/src/generic_imu_app.h
@@ -21,24 +21,21 @@
 #include "generic_imu_version.h"
 #include "hwlib.h"
 
-
 /*
 ** Specified pipe depth - how many messages will be queued in the pipe
 */
-#define GENERIC_IMU_PIPE_DEPTH            32
-
+#define GENERIC_IMU_PIPE_DEPTH 32
 
 /*
 ** Enabled and Disabled Definitions
 */
-#define GENERIC_IMU_DEVICE_DISABLED       0
-#define GENERIC_IMU_DEVICE_ENABLED        1
-
+#define GENERIC_IMU_DEVICE_DISABLED 0
+#define GENERIC_IMU_DEVICE_ENABLED  1
 
 /*
 ** GENERIC_IMU global data structure
-** The cFE convention is to put all global app data in a single struct. 
-** This struct is defined in the `generic_imu_app.h` file with one global instance 
+** The cFE convention is to put all global app data in a single struct.
+** This struct is defined in the `generic_imu_app.h` file with one global instance
 ** in the `.c` file.
 */
 typedef struct
@@ -47,8 +44,8 @@ typedef struct
     ** Housekeeping telemetry packet
     ** Each app defines its own packet which contains its OWN telemetry
     */
-    GENERIC_IMU_Hk_tlm_t   HkTelemetryPkt;   /* GENERIC_IMU Housekeeping Telemetry Packet */
-    
+    GENERIC_IMU_Hk_tlm_t HkTelemetryPkt; /* GENERIC_IMU Housekeeping Telemetry Packet */
+
     /*
     ** Operational data  - not reported in housekeeping
     */
@@ -58,26 +55,24 @@ typedef struct
     uint32_t RunStatus;
 
     /*
-	** Device data 
-	*/
-	// uint32 DeviceID;		            /* Device ID provided by CFS on initialization */
-    uint32_t DeviceID;
-    GENERIC_IMU_Device_tlm_t DevicePkt;      /* Device specific data packet */
+     ** Device data
+     */
+    // uint32 DeviceID;		            /* Device ID provided by CFS on initialization */
+    uint32_t                 DeviceID;
+    GENERIC_IMU_Device_tlm_t DevicePkt; /* Device specific data packet */
 
-    /* 
+    /*
     ** Device protocol: CAN
-    */ 
-    can_info_t Generic_imuCan;             /* Hardware protocol definition */
+    */
+    can_info_t Generic_imuCan; /* Hardware protocol definition */
 
 } GENERIC_IMU_AppData_t;
-
 
 /*
 ** Exported Data
 ** Extern the global struct in the header for the Unit Test Framework (UTF).
 */
 extern GENERIC_IMU_AppData_t GENERIC_IMU_AppData; /* GENERIC_IMU App Data */
-
 
 /*
 **
@@ -86,17 +81,17 @@ extern GENERIC_IMU_AppData_t GENERIC_IMU_AppData; /* GENERIC_IMU App Data */
 ** Note: Except for the entry point (IMU_AppMain), these
 **       functions are not called from any other source module.
 */
-void  IMU_AppMain(void);
+void IMU_AppMain(void);
 // int32 GENERIC_IMU_AppInit(void);
 int32_t GENERIC_IMU_AppInit(void);
-void  GENERIC_IMU_ProcessCommandPacket(void);
-void  GENERIC_IMU_ProcessGroundCommand(void);
-void  GENERIC_IMU_ProcessTelemetryRequest(void);
-void  GENERIC_IMU_ReportHousekeeping(void);
-void  GENERIC_IMU_ReportDeviceTelemetry(void);
-void  GENERIC_IMU_ResetCounters(void);
-void  GENERIC_IMU_Enable(void);
-void  GENERIC_IMU_Disable(void);
+void    GENERIC_IMU_ProcessCommandPacket(void);
+void    GENERIC_IMU_ProcessGroundCommand(void);
+void    GENERIC_IMU_ProcessTelemetryRequest(void);
+void    GENERIC_IMU_ReportHousekeeping(void);
+void    GENERIC_IMU_ReportDeviceTelemetry(void);
+void    GENERIC_IMU_ResetCounters(void);
+void    GENERIC_IMU_Enable(void);
+void    GENERIC_IMU_Disable(void);
 // int32 GENERIC_IMU_VerifyCmdLength(CFE_MSG_Message_t * msg, uint16 expected_length);
 
 #endif /* _GENERIC_IMU_APP_H_ */
