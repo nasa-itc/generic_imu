59ab14a9d584ca985a2c5cea35bafcfe35f796f0
dacarter22 <davidacarter22@gmail.com>
2025-04-08 11:23:19 -0400

    [nasa/nos3#581] removed cpt and lpt tests, increased test wait time to 60 seconds from 30 decreased margin from 0.2 to 0.1

--- gsw/GENERIC_IMU/lib/generic_imu_lib.rb
+++ gsw/GENERIC_IMU/lib/generic_imu_lib.rb
@@ -16,7 +16,6 @@ GENERIC_IMU_DEVICE_LOOP_COUNT = 5
 def get_generic_imu_hk()
     cmd("GENERIC_IMU GENERIC_IMU_REQ_HK")
     count = tlm("GENERIC_IMU GENERIC_IMU_HK_TLM CMD_COUNT")
-    puts "Command Count = #{count}"
     wait_check_packet("GENERIC_IMU", "GENERIC_IMU_HK_TLM", 1, GENERIC_IMU_RESPONSE_TIMEOUT)
     sleep(GENERIC_IMU_CMD_SLEEP)
 end
@@ -62,8 +61,6 @@ def enable_generic_imu()
 end
 
 def disable_generic_imu()
-    puts "Sending Disable command!"
-
     # Send command
     generic_imu_cmd("GENERIC_IMU GENERIC_IMU_DISABLE_CC")
     # Confirm
@@ -73,8 +70,6 @@ end
 def safe_generic_imu()
     get_generic_imu_hk()
     state = tlm("GENERIC_IMU GENERIC_IMU_HK_TLM DEVICE_ENABLED")
-    count = tlm("GENERIC_IMU GENERIC_IMU_HK_TLM CMD_COUNT")
-    puts "Command Count = #{count}"
     if (state != "DISABLED")
         disable_generic_imu()
     end
@@ -88,7 +83,7 @@ def confirm_generic_imu_data()
 
     get_generic_imu_data()
     # Note these checks assume default simulator configuration
-    diff = 0.2
+    diff = 0.1
 
     # X Axis
     imu_angular_acc_x = tlm("GENERIC_IMU GENERIC_IMU_DATA_TLM X_ANGULAR_ACCELERATION")
